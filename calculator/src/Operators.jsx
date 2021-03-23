1dcd5c969d53b3cbb4fb80d8433cc2b1069212ca
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-23 19:26:22 +0500

    calc

---
+++ calculator/src/Operators.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Operators(props){
+    return(<button value={props.val}>{props.val}</button>);
+}
+export default Operators
--- calculator/src/calc.jsx
+++ calculator/src/calc.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import Numbers from "./numbers"
+import Operators from "./Operators";
 function Calc(){
     return(
         <div className="Calc">
       <div className="Screen">
-
+        <input type="text"></input>
       </div>
       <div className="buttons">
         <div className="NumPad">
           <Numbers val="1"/>
           <Numbers val="2"/>
           <Numbers val="3"/>
+          <Operators val="X"/><br/>
           <Numbers val="4"/>
           <Numbers val="5"/>
           <Numbers val="6"/>
+          <Operators val="/"/><br/>
           <Numbers val="7"/>
           <Numbers val="8"/>
           <Numbers val="9"/>
+          <Operators val="+"/><br/>
           <Numbers val="."/>
           <Numbers val="0"/>
-          <Numbers val="="/>
+          <Operators val="="/>
+          <Operators val="-"/>
         </div>
         <div className="Operators">
 
