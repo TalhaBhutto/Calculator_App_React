ff6bfd25b71a503802bdd77a89357034b6d16aee
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-24 01:50:22 +0500

    Update Operators.jsx

--- calculator/src/Operators.jsx
+++ calculator/src/Operators.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 
 function Operators(props){
-     let target=document.getElementById("myscreen");
-    // target.value=target.value+props.val;
-    target.innerHTML=props.val;
     return(<button value={props.val}>{props.val}</button>);
 }
 export default Operators
